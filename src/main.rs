009d1053f3cd7f976f3210f1bb9327fe5595660d
rmgarcia <rockdrigo.mtz@gmail.com>
2018-11-13 17:40:26 -0600

    adding control flow, while

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 fn main() {
-   loop_sample();
+   //loop_sample();
+   while_sample();
 }
 
 fn loop_sample() {
     loop {
         println!("again!!");
     }
+}
+
+fn while_sample() {
+    let mut number = 5;
+
+    while number != 0 {
+        println!("{}!!", number);
+        number = number - 1;
+    }
+
+    println!("LIFTOFF!!!");
 }
